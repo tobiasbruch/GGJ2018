c05d94ee1fa6b6e8ea82ab8065c6562fd9b6e574
Marijn Zwemmer <marijn@marijnzwemmer.com>
2018-01-27 04:05:39 +0100

    Polish

--- Assets/Scripts/Player/ArrowCollision.cs
+++ Assets/Scripts/Player/ArrowCollision.cs
@@ -7,7 +7,7 @@ public class ArrowCollision : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D collision){
 		if(collision.gameObject.GetComponent<Arrow>()){
 			Destroy(gameObject);
-			Locator.Get<Game>().ShowGameOver();
+			Locator.Get<Game>().GameOver();
 		}
 	}
 }
