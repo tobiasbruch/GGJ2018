b70a591aa98cef49d9620497546f5f2183c9f0d5
Tobias Bruch <tobias.bruch@gmx.de>
2018-01-27 03:29:04 +0100

    Add basic game over screen

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour {
+	[SerializeField]
+	private GameObject _gameOverScreen;
+
+	[SerializeField]
+	private Canvas _uiCanvas;
 
 	// Use this for initialization
 	void Start ()
@@ -15,5 +20,9 @@ public class Game : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+	}
+
+	public void ShowGameOver(){
+		Instantiate(_gameOverScreen, _uiCanvas.transform, false);
 	}
 }
--- Assets/Scripts/Player/ArrowCollision.cs
+++ Assets/Scripts/Player/ArrowCollision.cs
@@ -7,7 +7,8 @@ public class ArrowCollision : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D collision){
 		if(collision.gameObject.GetComponent<Arrow>()){
 			Debug.Log("Player Hit by arrow");
-
+			Destroy(gameObject);
+			Locator.Get<Game>().ShowGameOver();
 		}
 	}
 }
