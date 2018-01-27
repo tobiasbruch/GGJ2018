4a5f62c74d0ffd6af48a17e8700f586a6f6e1190
Tobias Bruch <tobias.bruch@gmx.de>
2018-01-27 21:17:35 +0100

    Add wing flapping sounds

---
+++ Assets/Scripts/Player/PlayerSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour {
+
+	[SerializeField]
+	private AudioClip[] _wingFlapping;
+	[SerializeField]
+	private AudioSource _wingFlappingAudioSource;
+
+
+	public void PlayRandomWingFlapping(){
+		_wingFlappingAudioSource.PlayOneShot(_wingFlapping[Random.Range(0, _wingFlapping.Length)]);
+	}
+}
