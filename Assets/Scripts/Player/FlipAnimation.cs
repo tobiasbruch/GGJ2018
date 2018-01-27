a450a4f4454d5a8fb91db0aea3343dde35f422e0
Tobias Bruch <tobias.bruch@gmx.de>
2018-01-27 19:09:21 +0100

    Add pigeon asset

---
+++ Assets/Scripts/Player/FlipAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipAnimation : MonoBehaviour {
+
+	[SerializeField]
+	private SpriteRenderer _spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckForOrientation();
+	}
+
+	void CheckForOrientation(){
+		Debug.Log(transform.eulerAngles);
+		if(transform.eulerAngles.z < 0 || transform.eulerAngles.z > 180){
+			_spriteRenderer.flipY = true;
+		} else {
+			_spriteRenderer.flipY = false;
+		}
+	}
+}
