adab4d4e7c701726668059c5bd5ce143f3d4b8e4
Marijn Zwemmer <marijn@marijnzwemmer.com>
2018-01-27 22:54:08 +0100

    No end state anymore

--- Assets/Scripts/Player/ArrowCollision.cs
+++ Assets/Scripts/Player/ArrowCollision.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class ArrowCollision : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D collision){
-		if(collision.gameObject.GetComponent<Arrow>()){
-			Destroy(gameObject);
-			Locator.Get<Game>().GameOver();
+		var arrow = collision.gameObject.GetComponent<Arrow>();
+		if(arrow){
+			//Destroy(gameObject);
+			var r= collision.gameObject.GetComponent<Rigidbody2D>();
+			this.GetComponent<Rigidbody2D>().AddForce(r.velocity * 1.6f, ForceMode2D.Impulse);
+		//	Locator.Get<Game>().GameOver();
 		}
 	}
 }
